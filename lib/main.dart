6d4d86745d489d2f9686327c55d930b36a9e3498
Mohit Agrawal <37496789+mohitagrawal939@users.noreply.github.com>
2019-11-12 18:13:04 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/dashboard.dart';
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Dashboard',
-      theme: new ThemeData(primarySwatch: Colors.blueGrey, ),
+      theme: new ThemeData(primarySwatch: Colors.blueGrey),
       // home: new Dashboard(title: 'Flutter Demo Home Page'),
       home: Dashboard(),
       debugShowCheckedModeBanner: false,
